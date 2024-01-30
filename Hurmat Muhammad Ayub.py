a284a7251beb64a33515c4cf5529cf802f23a7fb
hurmatayub <156796678+hurmatayub@users.noreply.github.com>
2024-01-30 15:41:34 +0500

    Add files via upload

---
+++ Hurmat Muhammad Ayub.py
@@ -0,0 +1,12 @@
+##num_1 = float(input("Enter number 1:"))
+##num_2 = float(input("Enter number2:"))
+##
+##print("Avrage is :", num_1 + num_2/2)
+##              
+
+pie = 3.142
+radius = float(input("Enter area of circle:"))
+
+area= pie*radius*2
+
+print("Area of a circle:",%2:.f{area})
